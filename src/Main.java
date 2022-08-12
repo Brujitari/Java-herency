6075eb263c64d1957fdf154358363f759fc2383f
Brujitari <natimontesg2@mail.com>
2022-08-12 02:44:57 +0200

    improved prinln

--- src/Main.java
+++ src/Main.java
@@ -16,22 +16,20 @@ public class Main {
         Client myClient = new Client();
         myClient.age = 50;
         myClient.phone = "+34666666666";
-        myClient.name = "Rodrigo de la Rosa Ramírez";
+        myClient.name = "Rodrigo Ramirez";
         myClient.credit = 200000;
 
         Worker myWorker = new Worker();
         myWorker.age = 50;
         myWorker.phone = "+34888888888";
-        myWorker.name = "Rodrigo de la Rosa Ramírez";
+        myWorker.name = "Azura Garcia";
         myWorker.salary = 200000;
 
-        System.out.println(myClient.age);
-        System.out.println(myClient.phone);
-        System.out.println(myClient.name);
-        System.out.println(myClient.credit);
-        System.out.println(myWorker.age);
-        System.out.println(myWorker.phone);
-        System.out.println(myWorker.name);
-        System.out.println(myWorker.salary);
+        System.out.println("I am " + myClient.name + ", I am " + myClient.age +
+                " years old and my phone is " + myClient.phone
+                + " and my credit is " + myClient.credit + "$");
+        System.out.println("I am " + myWorker.name + ", I am " + myWorker.age +
+                " years old and my phone is " + myWorker.phone
+                + " and my salary is " + myWorker.salary + "$");
     }
 }
